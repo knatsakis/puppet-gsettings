1478b6db792eebf93b1fb044c103d53e526ff5eb
Konstantinos Natsakis <k.natsakis@xe.gr>
2016-08-17 20:31:55 +0300

    Install required development packages
    Main class installs packages required for building gem

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,7 @@
 class gsettings {
+  package { [ 'libglib2.0-dev', 'libgirepository1.0-dev' ]:
+    ensure => installed,
+  } ->
   package { 'gio2':
     ensure   => installed,
     provider => 'puppet_gem',
