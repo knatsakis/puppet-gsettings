2c6a427a9e5d1210cfa4e3dd4af04d60e8ea274d
Konstantinos Natsakis <5933427+knatsakis@users.noreply.github.com>
2019-03-29 11:33:04 +0200

    Improved array formatting

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,8 @@
 class gsettings {
-  package { [ 'libglib2.0-dev', 'libgirepository1.0-dev' ]:
+  package { [
+    'libglib2.0-dev',
+    'libgirepository1.0-dev',
+  ]:
     ensure => installed,
   } ->
   package { 'gio2':
