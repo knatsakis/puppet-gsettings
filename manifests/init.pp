18cb8c024f203e41fd9de6e4df2ad4371c3bbdc1
Konstantinos Natsakis <5933427+knatsakis@users.noreply.github.com>
2019-03-31 01:44:37 +0200

    Follow strict indentation rule

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,10 @@
 class gsettings {
-  package { [
-    'libglib2.0-dev',
-    'libgirepository1.0-dev',
-  ]:
-    ensure => installed,
+  package {
+    [
+      'libglib2.0-dev',
+      'libgirepository1.0-dev',
+    ]:
+      ensure => installed,
   } ->
   package { 'gio2':
     ensure   => latest,
