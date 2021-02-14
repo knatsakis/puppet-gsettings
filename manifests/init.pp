88909754a3324e82c6511b1300f39b3fb35434e3
Konstantinos Natsakis <5933427+knatsakis@users.noreply.github.com>
2021-02-14 08:58:19 +0200

    Use system's Ruby GIO2

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,5 @@
 class gsettings {
-  package {
-    'libglib2.0-dev':
-      ensure => installed;
-    'libgirepository1.0-dev':
-      ensure => installed;
-  } ->
-  package { 'gio2':
-    ensure   => installed,
-    provider => puppet_gem,
+  package { 'ruby-gio2':
+    ensure => installed,
   }
 }
